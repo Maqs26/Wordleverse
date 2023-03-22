dad84f09df5f059676d25a80991773931a2b28f8
Matthew Petschauer <Mpetschauer90@gmail.com>
2023-03-21 23:05:08 -0700

    Initial commit

---
+++ WordleDashboard/EFModels/MonthlyProgress.cs
@@ -0,0 +1,14 @@
+﻿namespace WordleDashboard.EFModels
+{
+    public class MonthlyProgress
+    {
+        public int Matt { get; set; }
+        public int Sandy { get; set; }
+        public int Marty { get; set; }
+        public int Sam { get; set; }
+        public int Andy { get; set; }
+        public int Angela { get; set; }
+        public int Days { get; set; }
+
+    }
+}
---
+++ WordleDashboard/EFModels/Player.cs
@@ -0,0 +1,29 @@
+﻿namespace WordleDashboard.EFModels
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? Round { get; set; }
+        public int? TotalRoundWins { get; set; }
+        public int? AvgGuessesPerRound { get; set; }
+        public int? TotalTies { get; set; }
+        public int? TotalRoundTies {get; set;}
+        public int? TotalGuesses { get; set; }
+        public decimal? AvgTotalGuesses { get; set; }
+        public decimal? Winpercentage { get; set; }
+
+        public double OneGuessInRound { get; set; }
+        public double TwoGuessesInRound { get; set; }
+        public double ThreeGuessesInRound { get; set; }
+        public double FourGuessesInRound { get; set; }
+        public double FiveGuessesInRound { get; set; }
+        public double SixGuessesInRound { get; set; }
+
+        public int? MissesInRound { get; set; }
+        public int? MissedEntriesTotal { get; set; }
+        public int? MissedEntriesInRound { get; set; }
+        public int? AvgMissedEntriesPerRound { get; set; }
+        public decimal? RoundWinPercentage { get; set; }
+    }
+}
